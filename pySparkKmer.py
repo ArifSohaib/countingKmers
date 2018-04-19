bc699b9cceeecf05c657fad2b5bf863b462a80c9
Sohaib <arif_sohaib@outlook.com>
2018-04-19 16:55:12 -0400

    kmers working, in sorted order

---
+++ pySparkKmer.py
@@ -0,0 +1,105 @@
+#Step 1: imports
+import findspark
+import argparse
+import os
+from operator import add
+import shutil
+import re
+findspark.init(
+    '/home/sohaib/Documents/MSCS/Big_Data/spark-2.3.0-bin-hadoop2.7/')
+import pyspark
+from pyspark.context import SparkContext
+
+
+
+def main():
+    #Step 2: handle input parameters
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    #filenames, K and N
+    parser.add_argument('-filenames', type=str, nargs='+',
+                    help='the list of fasta files', required=True)
+    parser.add_argument('-K', type=int, nargs=1, help='value of K in k-mer', required=True)
+    parser.add_argument('-N', type=int, nargs=1, help='value of N in top-n', required=True)
+    args = parser.parse_args()
+
+    #Step 3: create a Spark context object (ctx)
+    ctx = SparkContext("local", "Kmer count")
+
+    #Step 4: broadcast K and N as global shared objects
+    files = ctx.broadcast(args.filenames)
+    k = ctx.broadcast(args.K)
+    v = ctx.broadcast(args.N)
+    print(files.value)
+    print(k.value[0])
+    print(v.value[0])
+
+    #Step 5: read FASTQ file from HDFS and create the first RDD
+    records = ctx.textFile(files.value[0])
+    #remove file if exists
+    try:
+        shutil.rmtree("kmers/output/1")
+        print("removed old output")
+    except OSError:
+        print("kmers / output / 1 did not exist, creating now")
+    records.saveAsTextFile("kmers/output/1")
+    
+    #Step 6: filter redundant records
+    # firstChar = records.substr(0, 1)
+    filterRDD = records.filter(
+        lambda x: x[0:1] != '@' or x[0:1] != '+' or x[0:1] != ';' or x[0:1] != '!'or x[0:1] != '~')
+    try:
+        shutil.rmtree("kmers/output/1.5")
+        print("removed old output")
+    except OSError:
+        print("kmers / output / 1.5 did not exist, creating now")
+    filterRDD.saveAsTextFile("kmers/output/1.5")
+
+    #Step 7: generate K-mers
+    
+    #rec = records.collect()[0]
+    #kmer_lambda = lambda x: [(yield x[i:j]) for i , j in enumerate(range(0,count,k.value))]
+    #print(*kmer_lambda(rec))
+
+    #WORKS
+    kmers = records.map(
+            lambda x: (x[0:k.value[0]],1))
+    for val in kmers.collect():
+        print(val)
+    #/WORKS
+    # count = filterRDD.count()
+    # kmers = records.map(
+    #     lambda x: list([(yield x[i:k.value[0]+i]) for i in range(count)]))
+    try:
+        shutil.rmtree("kmers/output/2")
+        print("removed old output")
+    except OSError:
+        print("kmers / output / 2 did not exist, creating now")
+    kmers.saveAsTextFile("kmers/output/2")
+
+    # Step 8: Combine/reduce frequent K-mers
+    grouped = kmers.reduceByKey(add)
+    try:
+        shutil.rmtree("kmers/output/2.5")
+        print("removed old output")
+    except OSError:
+        print("kmers / output / 2.5 did not exist, creating now")
+    grouped.saveAsTextFile("kmers/output/2.5")
+    groups = grouped.collect()
+    for val in groups:
+        print(val)
+
+    # Step 9: create a local top N for all partitions
+    sortedKmers = grouped.map(lambda x: (int(-x[1]),x[0])).sortByKey().map(lambda x: (x[1], -1*int(x[0])))
+    try:
+        shutil.rmtree("kmers/output/3")
+        print("removed old output")
+    except OSError:
+        print("kmers / output / 3 did not exist, creating now")
+    sortedKmers.saveAsTextFile("kmers/output/3")
+    
+    #Step 10: get top N
+    
+
+if __name__ == '__main__':
+    main()
+    
