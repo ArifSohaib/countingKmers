cd752c23abd06eece099e1ffeb965875265c79f1
Sohaib <arif_sohaib@outlook.com>
2018-04-21 11:18:46 -0400

    fixed SPARK_HOME, added top-N

--- pySparkKmer.py
+++ pySparkKmer.py
@@ -5,8 +5,7 @@ import os
 from operator import add
 import shutil
 import re
-findspark.init(
-    '/home/sohaib/Documents/MSCS/Big_Data/spark-2.3.0-bin-hadoop2.7/')
+findspark.init(os.environ['SPARK_HOME'])
 import pyspark
 from pyspark.context import SparkContext
 
@@ -23,25 +22,26 @@ def main():
     args = parser.parse_args()
 
     #Step 3: create a Spark context object (ctx)
-    ctx = SparkContext("local", "Kmer count")
+    ctx = SparkContext(appName="Kmer count")
 
     #Step 4: broadcast K and N as global shared objects
     files = ctx.broadcast(args.filenames)
     k = ctx.broadcast(args.K)
-    v = ctx.broadcast(args.N)
+    n = ctx.broadcast(args.N)
     print(files.value)
-    print(k.value[0])
-    print(v.value[0])
+    print(k.value)
+    print(n.value)
+
 
     #Step 5: read FASTQ file from HDFS and create the first RDD
     records = ctx.textFile(files.value[0])
     #remove file if exists
-    try:
-        shutil.rmtree("kmers/output/1")
-        print("removed old output")
-    except OSError:
-        print("kmers / output / 1 did not exist, creating now")
-    records.saveAsTextFile("kmers/output/1")
+    # try:
+    #     shutil.rmtree("kmers/output/1")
+    #     print("removed old output")
+    # except OSError:
+    #     print("kmers / output / 1 did not exist, creating now")
+    # records.saveAsTextFile("kmers/output/1")
 
     #Step 6: filter redundant records
     # records = records.filter(lambda x: len(x)==70)
@@ -49,36 +49,36 @@ def main():
     filterRDD = records.filter(
         lambda x: x[0] != '>' or x[0] != ';' or x[0] != '@' or x[0] != '+' or x[0] != '!' or x[0] != '~')
 
-    for i in filterRDD.collect():
-        print(i)
-    try:
-        shutil.rmtree("kmers/output/1.5")
-        print("removed old output")
-    except OSError:
-        print("kmers / output / 1.5 did not exist, creating now")
-    filterRDD.saveAsTextFile("kmers/output/1.5")
+    # for i in filterRDD.collect():
+    #     print(i)
+    # try:
+    #     shutil.rmtree("kmers/output/1.5")
+    #     print("removed old output")
+    # except OSError:
+    #     print("kmers / output / 1.5 did not exist, creating now")
+    # filterRDD.saveAsTextFile("kmers/output/1.5")
 
     # Step 7: generate K-mers
-    
+    kVal = k.value[0]
     kmers = filterRDD.map(
-            lambda x: (x[0:3],1))
-    for k in kmers.collect():
-        print(k)
-    try:
-        shutil.rmtree("kmers/output/2")
-        print("removed old output")
-    except OSError:
-        print("kmers / output / 2 did not exist, creating now")
-    kmers.saveAsTextFile("kmers/output/2")
+            lambda x: (x[0:kVal],1))
+    # for k in kmers.collect():
+    #     print(k)
+    # try:
+    #     shutil.rmtree("kmers/output/2")
+    #     print("removed old output")
+    # except OSError:
+    #     print("kmers / output / 2 did not exist, creating now")
+    # kmers.saveAsTextFile("kmers/output/2")
 
     # Step 8: Combine/reduce frequent K-mers
-    grouped = kmers.reduceByKey(add)
-    try:
-        shutil.rmtree("kmers/output/2.5")
-        print("removed old output")
-    except OSError:
-        print("kmers / output / 2.5 did not exist, creating now")
-    grouped.saveAsTextFile("kmers/output/2.5")
+    grouped = kmers.reduceByKey(lambda x,y: x+y)
+    # try:
+    #     shutil.rmtree("kmers/output/2.5")
+    #     print("removed old output")
+    # except OSError:
+    #     print("kmers / output / 2.5 did not exist, creating now")
+    # grouped.saveAsTextFile("kmers/output/2.5")
 
     # Step 9: create a local top N for all partitions
     sortedKmers = grouped.map(lambda x: (int(-x[1]),x[0])).sortByKey().map(lambda x: (x[1], -1*int(x[0])))
@@ -90,7 +90,9 @@ def main():
     sortedKmers.saveAsTextFile("kmers/output/3")
     
     #Step 10: get top N
-
+    print("Top N={} {}-mers:".format(n.value[0],k.value[0]))
+    for val in sortedKmers.take(n.value[0]):
+        print(val)
 
 if __name__ == '__main__':
     main()
